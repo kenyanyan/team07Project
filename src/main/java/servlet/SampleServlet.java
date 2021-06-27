d6e5d29e16106be7d3936c24ae3c2359610d7cbb
kenyanyan <86397461+kenyanyan@users.noreply.github.com>
2021-06-27 18:53:43 +0900

    add samplefiles

---
+++ src/main/java/servlet/SampleServlet.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SampleServlet
+ */
+@WebServlet("/SampleServlet")
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SampleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO 自動生成されたメソッド・スタブ
+	
+	}
+
+	@Override
+	public void destroy() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
