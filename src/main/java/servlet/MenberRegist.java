5ad70e96bf80d7cab962028c44053b758f831f7d
takyoshizawa <takyoshizawa@SBT202102-0058.intra.softbank.co.jp>
2021-06-28 17:08:41 +0900

    会員登録機能のサーバー側のロジック

--- src/main/java/servlet/MenberRegist.java
+++ src/main/java/servlet/MenberRegist.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -29,8 +30,7 @@ public class MenberRegist extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		// 実行テストをやりたいときは、ここにフォワードする１７４ｐ参照
-		// パスを変える
+		
 		RequestDispatcher dispatcher =
 				request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
 		dispatcher.forward(request, response);
@@ -42,6 +42,61 @@ public class MenberRegist extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//会員登録時処理を書く
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String mail = request.getParameter("mail");
+		String pass = request.getParameter("pass");
+		String pass2 = request.getParameter("pass2");
+		String name = request.getParameter("name");
+		
+		boolean is_mail =true;
+		
+		
+		//System.out.println(pass.length()+"passの長さ");
+		
+		//パスワードのチェック
+		if (pass.length() < 8) {
+			System.out.println("8文字未満です");
+			is_mail = false;
+		}
+		
+		if (pass.equals(pass2)) {
+			System.out.println("passが一緒");
+		} 
+		else{
+			System.out.println("passが不一致");
+		}
+		
+		//ニックネームの空値チェック
+		
+		if (name.equals("")) {
+			System.out.println("ニックネームが入っていません");
+		} else{
+			System.out.println("ニックネームが入っています");
+		}
+		
+		//メールアドレス空値チェック
+		if (mail.equals("")) {
+			System.out.println("メールアドレスが入っていません");
+		} else{
+			System.out.println("メールアドレスが入っています");
+		}
+		//正規表現
+		
+		String pattern = "^([a-zA-Z0-9])+([a-zA-Z0-9\\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\\._-]+)+$";
+	    Pattern p = Pattern.compile(pattern);
+		
+	    if (p.matcher(mail).find()) {
+	        System.out.println(mail + "はメールアドレスです");
+	     } else {
+	        System.out.println(mail + "はメールアドレスではありません");
+	     }
+	
+		
+		
+		//System.out.println(mail);
+		//System.out.println(pass);
+		//System.out.println(pass2);
+		//System.out.println(name);
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/regist.jsp
+++ src/main/webapp/WEB-INF/jsp/regist.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,21 +8,43 @@
 <title>会員登録</title>
 </head>
 <body>
- <header class="page-header">
-            <h1><a href="index.html"><img class="logo" src="images/logo.png" alt="しゅみったー"></a></h1>
-            <nav>
-                <ul class="main-nav">
-                   
+	<header class="page-header">
+		<h1>
+			<a href="index.html"><img class="logo" src="images/logo.png"
+				alt="しゅみったー"></a>
+		</h1>
+		<nav>
+			<ul class="main-nav">
 
-                </ul>
-            </nav>
-        </header>
- <form name="myForm" method="post" action="study_member_add.html">
-        <h2 style="text-align:center"><label>ID:</label><input type="email" name="mail" placeholder="takuro1103y@gmail.com" required></h2>
-        <h2 style="text-align:center"><label>パスワード:</label><input type="password" name="pass" id="pass" required></h2>
-        <h2 style="text-align:center"><label>パスワード(確認):</label><input type="password" name="pass2" id="pass2" required></h2>
-        <h2 style="text-align:center"><label>ニックネーム:</label><input type="text"  name="name" placeholder="たくろう" required></h2>
-        <h2 style="text-align:center"><input type="submit" value="会員登録"></h2>
-    </form>
+
+			</ul>
+		</nav>
+	</header>
+	<div style="width: 30%; margin: 0 auto;">
+		<div>
+			<form name="myForm" method="post"
+				action="/team07Project/MenberRegist">
+				<h2 style="text-align: left">
+					<label>ID:</label><input type="email" name="mail"
+						placeholder="takuro1103y@gmail.com">
+				</h2>
+
+				<h2 style="text-align: left">
+					<label>パスワード:</label><input type="password" name="pass" id="pass">
+				</h2>
+				<h2 style="text-align: left">
+					<label>パスワード(確認):</label><input type="password" name="pass2"
+						id="pass2">
+				</h2>
+				<h2 style="text-align: left">
+					<label>ニックネーム:</label><input type="text" name="name"
+						placeholder="たくろう">
+				</h2>
+				<h2 style="text-align: left">
+					<input type="submit" value="会員登録">
+				</h2>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
