606c12b337d6aadfdd127b87950344e7707f6df0
kenyanyan <86397461+kenyanyan@users.noreply.github.com>
2021-06-28 17:15:45 +0900

    コンフリ

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -7,36 +7,39 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class Login
- */
+import model.User;
+
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		//ログイン認証処理を書く
-	}
 
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		User user = new User(id, pw);
+
+/*			LoginLogic loginLogic = new LoginLogic();
+			boolean isLogin = loginLogic.execute(user);*/
+				
+
+		boolean isLogin = true;
+		
+				if (isLogin) {
+					HttpSession session = request.getSession();
+					session.setAttribute("loginUser", user);
+				} else if (!isLogin == equals("")) {
+	}
+				request.getRequestDispatcher("/WEB-INF/jsp/main.jsp").forward(request, response);
+
+}
 }
