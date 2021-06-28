971ee6e40f55071b293169bb4d82398712321bf9
kobe_ <skobe@gmail.com>
2021-06-29 01:21:25 +0900

    Sql文の変更

--- src/main/java/dao/postDAO.java
+++ src/main/java/dao/postDAO.java
@@ -11,20 +11,23 @@ public class postDAO {
 	private final String JDBC_URL = "jdbc:h2:tcp://localhost/~/example";
 	private final String DB_USER = "sa";
 	private final String DB_pass = "";
-	
+
 	public boolean doPost(Post post) {
-	
+		System.out.println("doPost呼出し");
 		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_pass)) {
-			String sql = "INSERT INTO POST (CUSTOMER_ID,LARGE_ID,MIDDLE_ID,TITLE,TEXT,POST_TIME,IMAGE ) "
-					+ "VALUES('"+post.getCustomer_id()+"',"+post.getLarge_id()+","+post.getMiddle_id()+",'"+ post.getTitle() +"','"+post.getText() +"',"+ post.getPost_time() +","+post.getImage()+");";
+			System.out.println("DB connect");
+			String sql = "INSERT INTO POST (CUSTOMER_ID,LARGE_ID,MIDDLE_ID,TITLE,TEXT,IMAGE ) VALUES('"
+					+ post.getCustomer_id() + "'," + post.getLarge_id() + "," + post.getMiddle_id() + ",'"
+					+ post.getTitle() + "','" + post.getText() + "'," + post.getImage() + ");";
+			System.out.println(sql);
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-			 pStmt.executeUpdate();
-			 
+			pStmt.executeUpdate();
+
 		} catch (Exception e) {
 			return false;
 		}
 		return true;
 	}
-	
+
 }
