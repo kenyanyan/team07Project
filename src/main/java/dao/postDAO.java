1ad36ca747454a49430b231a77db065e369a5075
kobe_ <skobe@gmail.com>
2021-06-28 22:40:19 +0900

    postDAO作成

---
+++ src/main/java/dao/postDAO.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class postDAO {
+	private final String JDBC_URL = "jdbc:h2:tcp://localhost/~/example";
+	private final String DB_USER = "sa";
+	private final String DB_pass = "";
+	
+	
+	
+}
