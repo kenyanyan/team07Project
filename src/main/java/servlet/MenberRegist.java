7e2250bcb25da7bb1b86027db115b77cfa7a86aa
kenyanyan <86397461+kenyanyan@users.noreply.github.com>
2021-06-28 22:05:56 +0900

    登録画面の判定処理の記載

--- src/main/java/servlet/MenberRegist.java
+++ src/main/java/servlet/MenberRegist.java
@@ -16,87 +16,84 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MenberRegist")
 public class MenberRegist extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MenberRegist() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MenberRegist() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		RequestDispatcher dispatcher =
-				request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
+		request.setAttribute("is_mail", 0);
+		request.setAttribute("is_pass", true);
+		request.setAttribute("is_pass2", true);
+		request.setAttribute("is_name", true);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
 		dispatcher.forward(request, response);
-		}
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//会員登録時処理を書く
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 		String mail = request.getParameter("mail");
 		String pass = request.getParameter("pass");
 		String pass2 = request.getParameter("pass2");
 		String name = request.getParameter("name");
-		
-		boolean is_mail =true;
-		
-		
+
+		//入力チェックフラグ
+		int is_mail = 0; //正常 0  メールアドレス未入力 1  メールアドレスの書式があっていない 2 
+		boolean is_pass = true;
+		boolean is_pass2 = true;
+		boolean is_name = true;
+
+		//正規表現メール
+		String pattern = "^([a-zA-Z0-9])+([a-zA-Z0-9\\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\\._-]+)+$";
+		Pattern p = Pattern.compile(pattern);
+
 		//System.out.println(pass.length()+"passの長さ");
-		
+
 		//パスワードのチェック
 		if (pass.length() < 8) {
 			System.out.println("8文字未満です");
-			is_mail = false;
-		}
-		
-		if (pass.equals(pass2)) {
-			System.out.println("passが一緒");
-		} 
-		else{
+			is_pass = false;
+		} else if (!pass.equals(pass2)) {
 			System.out.println("passが不一致");
+			is_pass2 = false;
 		}
 		
 		//ニックネームの空値チェック
-		
 		if (name.equals("")) {
 			System.out.println("ニックネームが入っていません");
-		} else{
-			System.out.println("ニックネームが入っています");
+			is_name = false;
 		}
-		
+
 		//メールアドレス空値チェック
 		if (mail.equals("")) {
 			System.out.println("メールアドレスが入っていません");
-		} else{
-			System.out.println("メールアドレスが入っています");
+			is_mail = 1;
+		} else if (!p.matcher(mail).find()) {
+			is_mail = 2;
 		}
-		//正規表現
-		
-		String pattern = "^([a-zA-Z0-9])+([a-zA-Z0-9\\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\\._-]+)+$";
-	    Pattern p = Pattern.compile(pattern);
-		
-	    if (p.matcher(mail).find()) {
-	        System.out.println(mail + "はメールアドレスです");
-	     } else {
-	        System.out.println(mail + "はメールアドレスではありません");
-	     }
-	
-		
-		
-		//System.out.println(mail);
-		//System.out.println(pass);
-		//System.out.println(pass2);
-		//System.out.println(name);
+
+		//リクエストスコープにフラグ値登録
+		request.setAttribute("is_mail", is_mail);
+		request.setAttribute("is_pass", is_pass);
+		request.setAttribute("is_pass2", is_pass2);
+		request.setAttribute("is_name", is_name);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 }
