434be441d4dd0449186479e47b3956907a7d0677
ichiyasa-g <ichiyasa-g@example.com>
2021-06-28 16:07:39 +0900

    user.javaの変更

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,18 +4,4 @@ import java.io.Serializable;
 
 public class User implements Serializable {
 	//User用のBeans
-	String name;
-	String pass;
-	
-	public User() {
-		// TODO 自動生成されたコンストラクター・スタブ
-	}
-	
-	public User(String name, String pass) {
-		this.name = name;
-		this.pass = pass;
-		
-	}
-	public String getName() {return name;}
-	public String getPass() {return pass;}
 }
